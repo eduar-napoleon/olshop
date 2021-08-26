3316b81c177cd17d647107158ab40dc78d7fbc06
tantranesia <tantranesia@gmail.com>
2021-08-26 22:51:43 +0700

    feature, add to cart page

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,29 @@
-import React from 'react'
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import ProductDetail from "../view/ProductDetails"
-
+import ProductDetail from "../view/ProductDetails";
+import AddToCart from "../view/AddToCart"
 
 function index() {
-    return (
-        <div>
-            <Router>
-                <Switch>
-                <Route
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route
             key={1}
             exact={true}
             path={`/product-detail`}
             render={(props) => <ProductDetail {...props} />}
           />
-                </Switch>
-            </Router>
-            
-        </div>
-    )
+          <Route
+            key={1}
+            exact={true}
+            path={`/add-to-cart`}
+            render={(props) => <AddToCart {...props} />}
+          />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
-export default index
+export default index;
